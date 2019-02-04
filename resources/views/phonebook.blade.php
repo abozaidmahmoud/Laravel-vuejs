54125ab619023dd29c5963d589eb2ad9bcfca0cf
abozaidmahmoud <abozaidm50@yahoo.com>
2019-02-05 01:34:12 +0300

    first commit

---
+++ resources/views/phonebook.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF_8">
+        <title>vuejs</title>
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+
+    </head>
+    <body>
+        <div id="app">
+            <chead></chead>
+
+        </div>
+
+        <script src="{{asset('js/app.js')}}"></script>
+    </body>
+</head>
+</html>
